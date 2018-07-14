beb853c224afad00979c476715839cb78b40903c
GedMarc <ged_marc@hotmail.com>
2018-07-14 03:49:13 +0200

    JDK 10 Structure and Configuration

---
+++ src/jre10/java/module-info.java
@@ -0,0 +1,23 @@
+module com.jwebmp.plugins.jqplot {
+	exports com.jwebmp.plugins.jqplot;
+	exports com.jwebmp.plugins.jqplot.parts;
+	exports com.jwebmp.plugins.jqplot.parts.interfaces;
+	exports com.jwebmp.plugins.jqplot.options;
+	exports com.jwebmp.plugins.jqplot.options.axis;
+	exports com.jwebmp.plugins.jqplot.options.grid;
+	exports com.jwebmp.plugins.jqplot.options.legends;
+	exports com.jwebmp.plugins.jqplot.options.series;
+	exports com.jwebmp.plugins.jqplot.options.ticks;
+	exports com.jwebmp.plugins.jqplot.options.title;
+
+	exports com.jwebmp.plugins.jqplot.graphs;
+	exports com.jwebmp.plugins.jqplot.graphs.display;
+	exports com.jwebmp.plugins.jqplot.references;
+
+	requires com.jwebmp.core;
+	requires com.jwebmp.logmaster;
+	requires com.fasterxml.jackson.annotation;
+
+	requires java.validation;
+	requires java.logging;
+}
---
+++ src/main/java/com/jwebmp/plugins/jqplot/options/DefaultRendererOptions.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2017 Marc Magon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.jwebmp.plugins.jqplot.options;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.jwebmp.htmlbuilder.javascript.JavaScriptPart;
+import com.jwebmp.plugins.jqplot.JQPlotGraph;
+import com.jwebmp.plugins.jqplot.parts.interfaces.JQPlotAxisLabelRenderer;
+
+/**
+ * @author GedMarc
+ * @since 29 Feb 2016
+ */
+public class DefaultRendererOptions
+		extends JavaScriptPart
+		implements JQPlotAxisLabelRenderer
+{
+
+	private static final long serialVersionUID = 1L;
+
+	@JsonIgnore
+	private JQPlotGraph linkedGraph;
+
+	/**
+	 * The Default Axis Label Renderer Options
+	 *
+	 * @param linkedGraph
+	 */
+	public DefaultRendererOptions(JQPlotGraph linkedGraph)
+	{
+		this.linkedGraph = linkedGraph;
+	}
+
+	@Override
+	public String getRenderer()
+	{
+		return "";
+	}
+
+}
