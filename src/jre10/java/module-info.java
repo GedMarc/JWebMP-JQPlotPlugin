12bf8b05bfa1fcc11bd836fa6ccbeb08bf72182a
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:32 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,4 +1,6 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.jqplot.JQPlotPageConfigurator;
 
 module com.jwebmp.plugins.jqplot {
@@ -23,17 +25,21 @@ module com.jwebmp.plugins.jqplot {
 
 	requires java.validation;
 	requires java.logging;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with JQPlotPageConfigurator;
 
-	opens com.jwebmp.plugins.jqplot to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.jqplot.parts to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.jqplot.parts.interfaces to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.jqplot.options to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.jqplot.options.axis to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.jqplot.options.grid to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.jqplot.options.legends to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.jqplot.options.series to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.jqplot.options.ticks to com.fasterxml.jackson.databind,com.jwebmp.core;
-	opens com.jwebmp.plugins.jqplot.options.title to com.fasterxml.jackson.databind,com.jwebmp.core;
+	provides IGuiceScanModuleExclusions with com.jwebmp.plugins.jqplot.implementations.JQPlotExclusionsModule;
+	provides IGuiceScanJarExclusions with com.jwebmp.plugins.jqplot.implementations.JQPlotExclusionsModule;
+
+	opens com.jwebmp.plugins.jqplot to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.jqplot.parts to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.jqplot.parts.interfaces to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.jqplot.options to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.jqplot.options.axis to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.jqplot.options.grid to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.jqplot.options.legends to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.jqplot.options.series to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.jqplot.options.ticks to com.fasterxml.jackson.databind, com.jwebmp.core;
+	opens com.jwebmp.plugins.jqplot.options.title to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
---
+++ src/main/java/com/jwebmp/plugins/jqplot/implementations/JQPlotExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.jqplot.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class JQPlotExclusionsModule
+		implements IGuiceScanModuleExclusions<JQPlotExclusionsModule>,
+				           IGuiceScanJarExclusions<JQPlotExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-jqplot-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.jqplot");
+		return strings;
+	}
+}
