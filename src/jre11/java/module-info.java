8026ab9a062748e21e49346d96c4b362ac46bd84
GedMarc <ged_marc@hotmail.com>
2020-11-13 00:40:23 +0200

    module name java.validation to jakarta.validation

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -17,7 +17,7 @@ module com.jwebmp.plugins.jqplot {
 	requires com.jwebmp.core;
 	requires com.guicedee.logmaster;
 
-	requires java.validation;
+	requires jakarta.validation;
 	requires java.logging;
 	requires com.guicedee.guicedinjection;
 
